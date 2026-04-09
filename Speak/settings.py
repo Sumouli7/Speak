2ffdd6e4ee76a9722f6b64a26a35de44294354c7
Sumouli7 <sumouli05ece@gmail.com>
2026-04-09 20:41:55 +0530

    Fixed footer + static issue

--- Speak/settings.py
+++ Speak/settings.py
@@ -113,9 +113,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ---------------- MEDIA ---------------- #
 import os
