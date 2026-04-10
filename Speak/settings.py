220e83da4de0c83e9fe204086cda0ab31c89055d
Sumouli7 <sumouli05ece@gmail.com>
2026-04-11 04:03:51 +0530

    fixed database config for production

--- Speak/settings.py
+++ Speak/settings.py
@@ -87,12 +87,22 @@ TEMPLATES = [
 
 
 # ---------------- DATABASE ---------------- #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # ---------------- PASSWORD VALIDATION ---------------- #
