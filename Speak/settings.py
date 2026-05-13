6996622bcc54d2c9efe89b792082e5f8dd86e0c6
Sumouli7 <sumouli05ece@gmail.com>
2026-05-13 20:39:28 +0530

    Update settings.py

--- Speak/settings.py
+++ Speak/settings.py
@@ -89,24 +89,12 @@ TEMPLATES = [
 # ---------------- DATABASE ---------------- #
 import dj_database_url
 import os
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 
